b6a8d0b890872e68a20034cb9f1654791adf6e89
guillempiqueras <71658719+guillempiqueras@users.noreply.github.com>
2021-01-11 19:20:29 +0100

    practica final

--- practica_final/app/src/main/java/cat/copernic/guillem/piqueras/MainActivity.kt
+++ practica_final/app/src/main/java/cat/copernic/guillem/piqueras/MainActivity.kt
@@ -2,12 +2,40 @@ package cat.copernic.guillem.piqueras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu -> {
+                Toast.makeText(applicationContext, "Guillem Piqueras", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
 
     
 }
